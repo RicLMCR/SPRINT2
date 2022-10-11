eba6f3be1a27cda1bb008b3fa2d377f45e1f4c0c
Richard Larson <isit_ric@hotmail.com>
2022-10-11 16:41:49 +0100

    sample desk list

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const [deskList, setDeskList] = useState <any>([
+  //   { id: "1", name: "desk1", user: "", booked: false },
+  //   { id: "2",name: "desk2", user: "", booked: false },
+  //   { id: "3", name: "desk3", user: "", booked: false },
+  // ]);
+
+const [myArr, setMyArr]=useState([])
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+
+</div>
   );
 }
 
